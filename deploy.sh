4b50e6573dcf10f158dd2ab569fe0e61d9059d0c
Melissa Smith <melissasmith@Kens-MacBook-Air.local>
2026-09-09 22:00:17 -0700

    Auto commit

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,5 @@ aws s3 sync \
    ./ \
    s3://www.melissasmithphotography.com/ \
    --profile greyarea
+   
+aws cloudfront create-invalidation --distribution-id E1RLOE638VT4JU --paths "/*" --profile greyarea
